14a72a37d991368b8be9521a7f7eed5bc6903c46
Stephane Liu <sliu@sjliu.com>
2020-03-15 16:25:41 -1000

    Feat: Add install of extensions

--- install.sh
+++ install.sh
@@ -5,6 +5,27 @@ if [[ $# -eq 0 ]]; then
   exit 1
 fi
 
+# If install is slow, make sure telemetry is turned off
+# settings.json disables this as well
+code --force --install-extension alexdima.copy-relative-path
+code --force --install-extension DavidAnson.vscode-markdownlint
+code --force --install-extension dbaeumer.vscode-eslint
+code --force --install-extension esbenp.prettier-vscode
+code --force --install-extension extr0py.vscode-relative-line-numbers
+code --force --install-extension ginfuru.ginfuru-better-solarized-dark-theme
+code --force --install-extension lucaslpsan.vuetify-pug-snippet
+code --force --install-extension mechatroner.rainbow-csv
+code --force --install-extension ms-azuretools.vscode-docker
+code --force --install-extension msjsdiag.debugger-for-chrome
+code --force --install-extension octref.vetur
+code --force --install-extension rebornix.ruby
+code --force --install-extension sdras.night-owl
+code --force --install-extension sdras.vue-vscode-snippeets
+code --force --install-extension vscodevim.vim
+code --force --install-extension WallabyJs.quokka-vscode
+code --force --install-extension wingrunr21.vscode-ruby
+# code --force --install-extension
+
 install_dest=$1
 
 if [ ! -d $install_dest/.vscode ]; then
